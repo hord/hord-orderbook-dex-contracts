ee0203226cba90e86e24068dac400380210d0092
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2021-11-16 19:27:54 +0100

    use ethers parseEther function in deployment script to avoid BigNumber error

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -36,7 +36,7 @@ async function main() {
   const matchingMarket = await ethers.getContractFactory("MatchingMarket");
   const MatchingMarket = await matchingMarket.deploy(
     "0x8301f2213c0eed49a7e28ae4c3e91722919b8b47", // Testnet BUSD address dust token
-    100000000000000000000, // 100 dust limit
+    ethers.utils.parseEther("100"), // 100 dust limit (100000000000000000000 in wei), BUSD has 18 decimals (https://testnet.bscscan.com/token/0x8301f2213c0eed49a7e28ae4c3e91722919b8b47)
     UniswapSimplePriceOracle.address // from step 1 deployed UniswapSimplePriceOracle
   );
   console.log(`MatchingMarket address: ${MatchingMarket.address}`);
