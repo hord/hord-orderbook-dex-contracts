e53d1cb0f9dc8cc8081624a736f5dd68378aa943
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2021-11-16 18:57:59 +0100

    log deployers address and balance on deployment

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,23 +3,24 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
+const fs = require("fs");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const [deployer] = await ethers.getSigners();
+  console.log(`Deploying contracts with the account: ${deployer.address}`);
 
-  // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const balance = await deployer.getBalance();
+  console.log(`Account balance: ${balance.toString()}`);
 
-  await greeter.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  // // We get the contract to deploy
+  // const MatchingMarket = await ethers.getContractFactory("MatchingMarket");
+  // const matchingMarket = await MatchingMarket.deploy("parameter");
+
+  // await matchingMarket.deployed();
+
+  // console.log("matchingMarket deployed to:", matchingMarket.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
