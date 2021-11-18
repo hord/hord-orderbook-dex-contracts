173bc64c8f9303401a31f9127602037986a982fc
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2021-11-18 22:40:40 +0100

    create OtcInterface

---
+++ contracts/interfaces/OtcInterface.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.5.12;
+
+contract OtcInterface {
+    struct OfferInfo {
+        uint              pay_amt;
+        address           pay_gem;
+        uint              buy_amt;
+        address           buy_gem;
+        address           owner;
+        uint64            timestamp;
+    }
+    mapping (uint => OfferInfo) public offers;
+    function getBestOffer(address, address) public view returns (uint);
+    function getWorseOffer(uint) public view returns (uint);
+}
