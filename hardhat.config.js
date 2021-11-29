2a1c5aa663bf30b7bcd5cb2f6f22ea7116928825
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2021-11-29 15:18:18 +0100

    change solidity version to 0.8.10 in config file

--- hardhat.config.js
+++ hardhat.config.js
@@ -22,7 +22,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.5.12",
+  solidity: "0.8.10",
   networks: {
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
