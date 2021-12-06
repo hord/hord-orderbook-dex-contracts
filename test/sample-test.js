dc68ed4b1cb095ba9d8add5e08552c99934be7b6
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2021-12-06 13:52:38 +0100

    add passing test

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,19 +1,36 @@
-const { expect } = require("chai");
+// const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+let MatchingMarketContract, owner, ownerAddr;
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+async function setupContractAndAccounts () {
+  let accounts = await ethers.getSigners()
+  owner = accounts[0]
+  ownerAddr = await owner.getAddress()
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+  const MatchingMarket = await hre.ethers.getContractFactory("MatchingMarket");
+  MatchingMarketContract = await MatchingMarket.deploy(
+  );
+  await MatchingMarketContract.deployed()
+}
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+describe("MatchingMarket", function () {
+  before('setup MatchingMarket contract', async () => {
+    await setupContractAndAccounts()
+  })
+  
+  it("test", async function () {
+    // const Greeter = await ethers.getContractFactory("Greeter");
+    // const greeter = await Greeter.deploy("Hello, world!");
+    // await greeter.deployed();
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    // expect(await greeter.greet()).to.equal("Hello, world!");
+
+    // const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+
+    // // wait until the transaction is mined
+    // await setGreetingTx.wait();
+
+    // expect(await greeter.greet()).to.equal("Hola, mundo!");
   });
 });
