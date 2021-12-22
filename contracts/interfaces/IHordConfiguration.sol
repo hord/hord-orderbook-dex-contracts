b2d416239ea9daf7f572ae8a6a367db221426951
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2021-12-22 18:32:48 +0100

    remove hordconfiguration contracr

--- contracts/interfaces/IHordConfiguration.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity 0.8.10;
-
-interface IHordConfiguration {
-    function hordToken() external view returns(address);
-    function exitFeeAmount(uint256 usdAmountWei) external view returns (uint256);
-}
