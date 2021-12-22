edeed7bdc597f1d11c0b212440e2cef11ad2a004
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2021-12-22 22:46:43 +0100

    expose dusttoken and dustlimit func in config interface

--- contracts/interfaces/IOrderbookConfiguration.sol
+++ contracts/interfaces/IOrderbookConfiguration.sol
@@ -2,5 +2,7 @@ pragma solidity 0.8.10;
 
 interface IOrderbookConfiguration {
     function hordToken() external view returns(address);
+    function dustToken() external view returns(address);
+    function dustLimit() external view returns (uint256);
     function exitFeeAmount(uint256 usdAmountWei) external view returns (uint256);
 }
