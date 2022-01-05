9242188d90efa942a40c9e6caa68e637098148f7
s2imonovic <s2imonovic@gmail.com>
2022-01-05 14:36:12 +0100

    Add hardhat-contract-sizer tool.

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 require("@nomiclabs/hardhat-etherscan");
 require("@nomiclabs/hardhat-waffle");
 require("hardhat-gas-reporter");
+require("hardhat-contract-sizer");
 require("solidity-coverage");
 require('@openzeppelin/hardhat-upgrades')
 
