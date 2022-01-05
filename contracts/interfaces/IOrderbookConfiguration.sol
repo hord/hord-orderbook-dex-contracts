790fbd0cf9210305e208bb2a9e7a3628f691c3b4
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2022-01-05 18:48:12 +0100

    add calculate fee functions to interface

--- contracts/interfaces/IOrderbookConfiguration.sol
+++ contracts/interfaces/IOrderbookConfiguration.sol
@@ -4,5 +4,7 @@ interface IOrderbookConfiguration {
     function hordToken() external view returns(address);
     function dustToken() external view returns(address);
     function dustLimit() external view returns (uint256);
-    function exitFeeAmount(uint256 usdAmountWei) external view returns (uint256);
+    function calculateTotalFee(uint256 amount) external view returns (uint256);
+    function calculateChampionFee(uint256 amount) external view returns (uint256);
+    function calculateOrderbookFee(uint256 amount) external view returns (uint256);
 }
