942922c17fb774d1061bb50f274ccf2e1d6dc092
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2022-01-05 10:06:15 +0100

    add interface for HPool

--- contracts/SimpleMarket.sol
+++ contracts/SimpleMarket.sol
@@ -4,7 +4,6 @@
 
 // Copyright (C) 2016 - 2021 Dai Foundation
 
-//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
