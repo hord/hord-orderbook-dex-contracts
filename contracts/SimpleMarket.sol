cd46224c54cbea69ab7a8825c34779184a74ea2c
s2imonovic <s2imonovic@gmail.com>
2022-01-19 13:05:19 +0100

    Wip.

--- contracts/SimpleMarket.sol
+++ contracts/SimpleMarket.sol
@@ -214,8 +214,50 @@ contract SimpleMarket is EventfulMarket, DSMath, OrderBookUpgradable, PausableUp
 
         offers[id].pay_amt = sub(offer.pay_amt, quantity);
         offers[id].buy_amt = sub(offer.buy_amt, spend);
-        safeTransferFrom(offer.buy_gem, msg.sender, offer.owner, spend);
-        safeTransfer(offer.pay_gem, msg.sender, quantity);
+
+        if (address(offer.buy_gem) == address(dustToken)) { // offer.buy_gem is BUSD
+            uint256 totalFee = orderbookConfiguration.calculateTotalFee(spend);
+            uint256 championFee = orderbookConfiguration.calculateChampionFee(totalFee);
+            uint256 protocolFee = orderbookConfiguration.calculateOrderbookFee(totalFee);
+
+            uint256 updatedSpend = spend - (championFee + protocolFee); // take champion and protocol fee from BUSD
+
+            platformFee.feesAvailable = platformFee.feesAvailable + protocolFee; // add taken protocol fee to keep track of total fees on the contract
+
+            address championAddress = IHPool(address(offer.pay_gem)).hPool().championAddress;
+
+            safeTransferFrom(offer.buy_gem, msg.sender, championAddress, championFee); // send champion fee to champion
+            safeTransferFrom(offer.buy_gem, msg.sender, address(this), protocolFee); // send protocol fee to this address
+
+            emit FeesTaken(
+                championFee,
+                protocolFee
+            );
+            safeTransferFrom(offer.buy_gem, msg.sender, offer.owner, updatedSpend);
+            safeTransfer(offer.pay_gem, msg.sender, quantity);
+
+        } else { // offer.pay_gem is BUSD
+            uint256 totalFee = orderbookConfiguration.calculateTotalFee(quantity);
+            uint256 championFee = orderbookConfiguration.calculateChampionFee(totalFee);
+            uint256 protocolFee = orderbookConfiguration.calculateOrderbookFee(totalFee); // In this condition the protocol fee already is on orderbook contract, so we dont need to transfer BUSD to it
+
+            uint256 updatedQuantity = quantity - (championFee + protocolFee); // take champion and protocol fee from BUSD
+
+            address championAddress = IHPool(address(offer.buy_gem)).hPool().championAddress;
+
+            platformFee.feesAvailable = platformFee.feesAvailable + protocolFee; // add taken protocol fee to keep track of total fees on the contract
+
+
+            safeTransfer(offer.pay_gem, championAddress, championFee);// send champion fee to champion
+
+            emit FeesTaken(
+                championFee,
+                protocolFee
+            );
+            safeTransferFrom(offer.buy_gem, msg.sender, offer.owner, spend);
+            safeTransfer(offer.pay_gem, msg.sender, updatedQuantity);
+        }
+
 
         emit LogItemUpdate(id);
         emit LogTake(
