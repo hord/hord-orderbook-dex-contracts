983482025cd36fabf37bbed3010bba8e3b318ebf
s2imonovic <s2imonovic@gmail.com>
2022-01-21 10:15:34 +0100

    Change saveAbis script.

--- scripts/saveAbis.js
+++ scripts/saveAbis.js
@@ -7,8 +7,9 @@ let c = require('../deployments/deploymentConfig.json');
 async function main() {
     await hre.run('compile');
 
-    saveContractAbi(hre.network.name, 'HordToken', (await hre.artifacts.readArtifact("MatchingMarket")).abi)
-    saveContractAbi(hre.network.name, 'HordTicketManager', (await hre.artifacts.readArtifact("OrderBookConfiguration")).abi)
+    saveContractAbi(hre.network.name, 'MatchingMarket', (await hre.artifacts.readArtifact("MatchingMarket")).abi)
+    saveContractAbi(hre.network.name, 'OrderBookConfiguration', (await hre.artifacts.readArtifact("OrderBookConfiguration")).abi)
+    saveContractAbi(hre.network.name, 'MakerOtcSupportMethods', (await hre.artifacts.readArtifact("MakerOtcSupportMethods")).abi)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
