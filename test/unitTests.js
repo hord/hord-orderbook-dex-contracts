746e701b9e6e4698cc78b718c38f25ee71d3d98c
s2imonovic <s2imonovic@gmail.com>
2023-01-12 21:47:16 +0100

    Test DEX

--- test/unitTests.js
+++ test/unitTests.js
@@ -11,8 +11,8 @@ let config;
 let accounts, owner, ownerAddr, hordCongress, hordCongressAddr, hPoolManager, maintainersRegistry,
     hordToken, dustToken, hETHToken, maintainer, maintainerAddr, matchingMarket, orderBookConfiguration,
     hordTreasury, makerOtcSupportMethods;
-let vPoolManager, user1, user1Addr, user2, user2Addr, user3, user3Addr, user4, user4Addr, user5, user5Addr;
-let dustLimit = 1, totalFeePercent = 25000, percentPrecision = 1000000;
+let vPoolManager, user1, user1Addr, user2, user2Addr, user3, user3Addr, user4, user4Addr, user5, user5Addr, user6Addr,
+    user6, user7Addr, user7;
 
 async function setupContractAndAccounts () {
     config = configuration[hre.network.name];
@@ -24,16 +24,20 @@ async function setupContractAndAccounts () {
     hordCongressAddr = await hordCongress.getAddress();
     maintainer = accounts[2];
     maintainerAddr = await maintainer.getAddress();
-    user1 = accounts[2];
+    user1 = accounts[3];
     user1Addr = await user1.getAddress();
-    user2 = accounts[2];
+    user2 = accounts[4];
     user2Addr = await user2.getAddress();
-    user3 = accounts[2];
+    user3 = accounts[5];
     user3Addr = await user3.getAddress();
-    user4 = accounts[2];
+    user4 = accounts[6];
     user4Addr = await user4.getAddress();
-    user5 = accounts[2];
+    user5 = accounts[7];
     user5Addr = await user5.getAddress();
+    user6 = accounts[8];
+    user6Addr = await user6.getAddress();
+    user7 = accounts[9];
+    user7Addr = await user7.getAddress();
 
 
     const MaintainersRegistry = await ethers.getContractFactory('MockMaintainersRegistry');
@@ -132,57 +136,100 @@ describe('MatchingMarket', async() => {
         });
 
         it('should send some hETH and USD to users', async() => {
-            await dustToken.connect(owner).transfer(user1Addr, toHordDenomination(100000));
-            await dustToken.connect(owner).transfer(user2Addr, toHordDenomination(100000));
-            await dustToken.connect(owner).transfer(user3Addr, toHordDenomination(100000));
-            await dustToken.connect(owner).transfer(user4Addr, toHordDenomination(100000));
-            await dustToken.connect(owner).transfer(user5Addr, toHordDenomination(100000));
+            await dustToken.connect(owner).transfer(user1Addr, toHordDenomination(10000));
+            await dustToken.connect(owner).transfer(user2Addr, toHordDenomination(10000));
+            await dustToken.connect(owner).transfer(user3Addr, toHordDenomination(10000));
+            await dustToken.connect(owner).transfer(user4Addr, toHordDenomination(10000));
+            await dustToken.connect(owner).transfer(user5Addr, toHordDenomination(10000));
+            await dustToken.connect(owner).transfer(user6Addr, toHordDenomination(10000));
+            await dustToken.connect(owner).transfer(user7Addr, toHordDenomination(10000));
 
-            await hETHToken.connect(owner).transfer(user1Addr, toHordDenomination(1000));
-            await hETHToken.connect(owner).transfer(user2Addr, toHordDenomination(1000));
-            await hETHToken.connect(owner).transfer(user3Addr, toHordDenomination(1000));
-            await hETHToken.connect(owner).transfer(user4Addr, toHordDenomination(1000));
-            await hETHToken.connect(owner).transfer(user5Addr, toHordDenomination(1000));
+            await hETHToken.connect(owner).transfer(user1Addr, toHordDenomination(1));
+            await hETHToken.connect(owner).transfer(user2Addr, toHordDenomination(1));
+            await hETHToken.connect(owner).transfer(user3Addr, toHordDenomination(1));
+            await hETHToken.connect(owner).transfer(user4Addr, toHordDenomination(1));
+            await hETHToken.connect(owner).transfer(user5Addr, toHordDenomination(1));
+            await hETHToken.connect(owner).transfer(user6Addr, toHordDenomination(1));
+            await hETHToken.connect(owner).transfer(user7Addr, toHordDenomination(1));
         });
     });
 
-    describe('MatchingMarket::Offer', async() => {
-        it('should make some sell offer', async() => {
-            await hETHToken.connect(user1).approve(matchingMarket.address, toHordDenomination(10));
-            await hETHToken.connect(user3).approve(matchingMarket.address, toHordDenomination(10));
-            await hETHToken.connect(user5).approve(matchingMarket.address, toHordDenomination(10));
+    // First make 2 buy offers A: 1200$-1HETH and B: 100$-1HETH
+    describe('MatchingMarket::FirstCase', async() => {
+        it('should make some buy offers', async() => {
+            await dustToken.connect(user1).approve(matchingMarket.address, toHordDenomination(12000));
+            await dustToken.connect(user3).approve(matchingMarket.address, toHordDenomination(1000));
 
             await matchingMarket.connect(user1).offer(
+                toHordDenomination(1200),
+                dustToken.address,
                 toHordDenomination(1),
                 hETHToken.address,
-                toHordDenomination(100),
-                dustToken.address,
                 0
             );
 
             await matchingMarket.connect(user3).offer(
-                toHordDenomination(1),
-                hETHToken.address,
-                toHordDenomination(200),
-                dustToken.address,
-                0
-            );
-
-            await matchingMarket.connect(user5).offer(
-                toHordDenomination(1),
-                hETHToken.address,
                 toHordDenomination(100),
                 dustToken.address,
+                toHordDenomination(1),
+                hETHToken.address,
                 0
             );
 
         });
 
-        it('should make some buy offer', async() => {
-            await dustToken.connect(user2).approve(matchingMarket.address, toHordDenomination(10000));
+        it('should make sell offer', async() => {
+            await hETHToken.connect(user2).approve(matchingMarket.address, toHordDenomination(1));
 
             await matchingMarket.connect(user2).offer(
+                toHordDenomination(1),
+                hETHToken.address,
                 toHordDenomination(100),
+                dustToken.address,
+                0
+            );
+
+        });
+
+        it('should cancel all offers from first case', async() => {
+            await matchingMarket.connect(user3).cancel(2);
+        });
+
+    });
+
+    describe('MatchingMarket::SecondCase', async() => {
+        it('should make some sell offers', async() => {
+            await hETHToken.connect(user4).approve(matchingMarket.address, toHordDenomination(1));
+            await hETHToken.connect(user6).approve(matchingMarket.address, toHordDenomination(1));
+
+            await matchingMarket.connect(user4).offer(
+                toHordDenomination(1),
+                hETHToken.address,
+                toHordDenomination(100),
+                dustToken.address,
+                0
+            );
+
+            await matchingMarket.connect(user6).offer(
+                toHordDenomination(1),
+                hETHToken.address,
+                toHordDenomination(1500),
+                dustToken.address,
+                0
+            );
+
+        });
+
+        it('s', async() => {
+            let a = await makerOtcSupportMethods.getOffers(matchingMarket.address, hETHToken.address, dustToken.address);
+            console.log(a);
+        });
+
+        it('should make buy offer', async() => {
+            await dustToken.connect(user5).approve(matchingMarket.address, toHordDenomination(1500));
+
+            await matchingMarket.connect(user5).offer(
+                toHordDenomination(1500),
                 dustToken.address,
                 toHordDenomination(1),
                 hETHToken.address,
@@ -190,6 +237,12 @@ describe('MatchingMarket', async() => {
             );
 
         });
+
+        it('s', async() => {
+            let a = await makerOtcSupportMethods.getOffers(matchingMarket.address, hETHToken.address, dustToken.address);
+            console.log(a);
+        });
+
     });
 
 });
